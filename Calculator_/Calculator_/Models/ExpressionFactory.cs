3a8f3f0bb0e6e7cce728b796e0f331202fe5fb17
MSamoukovic <MSamoukovic>
2019-12-25 20:51:46 +0100

    classes: Expr, ExprFactry, AddExpr, SubstractExpr, MultiplyExpr, DivideExpr

---
+++ Calculator_/Calculator_/Models/ExpressionFactory.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator_.Models
+{
+    class ExpressionFactory
+    {
+        public static Expr getExpression(string operation)
+        {
+            switch (operation)
+            {
+                case ("+"):
+                    return new AddExpr();
+                case ("-"):
+                    return new SubtractExpr();
+                case ("*"):
+                    return new MultiplyExpr();
+                default:
+                    return new DivideExpr();
+            }
+        }
+    }
+}
---
+++ Calculator_/Calculator_/Models/SubstractExpr.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator_.Models
+{
+    class SubstractExpr : Expr
+    {
+        public override double getResult(Operand firstOperand, Operand secondOperand)
+        {
+            double result = firstOperand.getNumber() / secondOperand.getNumber();
+            return result;
+        }
+    }
+}
