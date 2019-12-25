b37172365572a5d337b45265e31a91055ad6ce15
MSamoukovic <MSamoukovic>
2019-12-25 20:43:07 +0100

    classes: Operator, Operand

---
+++ Calculator_/Calculator_/Models/Operand.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator_.Models
+{
+    class Operand
+    {
+        public double Number = 0.0;
+
+        public Operand(double number)
+        {
+            Number = number;
+        }
+        public Operand(string numberText)
+        {
+            if (String.IsNullOrEmpty(numberText))
+            {
+                Number = 0.0;
+            }
+            else
+            {
+                Number = Double.Parse(numberText);
+            }
+        }
+        public double getNumber()
+        {
+            return Number;
+        }
+        public void setNumber(Double Number)
+        {
+            this.Number = Number;
+        }
+    }
+}
---
+++ Calculator_/Calculator_/Models/Operator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator_.Models
+{
+    class Operator
+    {
+        public string operation;
+        public Operator()
+        {
+
+        }
+        public Operator(string operatorButtonText)
+        {
+            if (String.IsNullOrEmpty(operatorButtonText))
+            {
+                operation = "+";
+            }
+            else
+            {
+                operation = operatorButtonText;
+            }
+        }
+        public string getOperator()
+        {
+            return operation;
+        }     
+    }
+}
